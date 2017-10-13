a8236b8634d8268dc7d4b1680cdefdf75f17a4dc
Darren Camp <darren@codingintent.com>
2017-10-13 21:16:29 +1030

    Initial Commit

---
+++ CodingIntent.Contrib/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingIntent.Contrib
+{
+    public static class StringExtensions
+    {
+		public static string[] SplitOnNewLine(this string source)
+		{
+			return source.Split(new[] { "\r\n", "\n", "\r" }, StringSplitOptions.None);
+		}
+	}
+}
