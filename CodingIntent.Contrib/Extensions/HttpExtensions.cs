632dfbb6ba574b47e615f970b82689aa1f83f284
Darren Camp <darren@codingintent.com>
2017-10-14 14:48:55 +1030

    Add HttpExtensions class

---
+++ CodingIntent.Contrib/Extensions/HttpExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace CodingIntent.Contrib.Extensions
+{
+    public static class HttpExtensions
+    {
+		public static T Fetch<T>(this HttpClient client)
+		{
+			return default(T);
+		}
+	}
+}
