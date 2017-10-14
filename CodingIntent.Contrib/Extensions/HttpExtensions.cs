3af8cbdaf89ab592cb14fbda79dc9f2ce686121e
Darren Camp <darren@codingintent.com>
2017-10-14 15:50:31 +1030

    Fix whitespace

--- CodingIntent.Contrib/Extensions/HttpExtensions.cs
+++ CodingIntent.Contrib/Extensions/HttpExtensions.cs
@@ -5,8 +5,8 @@ using System.Text;
 
 namespace CodingIntent.Contrib.Extensions
 {
-    public static class HttpExtensions
-    {
+	public static class HttpExtensions
+	{
 		public static T Fetch<T>(this HttpClient client)
 		{
 			return default(T);
